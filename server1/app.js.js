286b5d193b56286ea55f7e0483f0100a63346cf8
Ashish Rawat <ashish09121997@gmail.com>
2023-08-07 19:20:51 +0530

    server1 and server2 added

---
+++ server1/app.js.js
@@ -0,0 +1,300 @@
+var express = require("express");
+var app = express();
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE, HEAD"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+const port = 2410;
+app.listen(port, () => console.log(`Node app listening on port ${port}!`));
+let { AData } = require("./Data");
+let fs = require("fs");
+let fname = "data.json";
+
+app.get("/resetData", function (req, res) {
+  let data = JSON.stringify(AData);
+  fs.writeFile(fname, data, function (err) {
+    if (err) res.status(404).send(err);
+    else res.send("Data in file is reset");
+  });
+});
+app.get("/shops", function (req, res) {
+  fs.readFile(fname, "utf-8", function (err, data) {
+    if (err) res.status(404).send(err);
+    else {
+      let rData = JSON.parse(data);
+      let r1 = rData.shops.map((curr) => ({
+        shopid: curr.shopId,
+        shopname: curr.name,
+        rent: curr.rent,
+      }));
+
+      res.send(r1);
+    }
+  });
+});
+app.get("/products", function (req, res) {
+  fs.readFile(fname, "utf-8", function (err, data) {
+    if (err) res.status(404).send(err);
+    else {
+      let rData = JSON.parse(data);
+      let r1 = rData.products.map((curr) => ({
+        productid: curr.productId,
+        productname: curr.productName,
+        category: curr.category,
+        description: curr.description,
+      }));
+      res.send(r1);
+    }
+  });
+});
+app.get("/products/:id", function (req, res) {
+  let id = +req.params.id;
+  fs.readFile(fname, "utf-8", function (err, data) {
+    if (err) res.status(404).send(err);
+    else {
+      let rData = JSON.parse(data);
+      let index = rData.products.findIndex((ct) => ct.productId == id);
+      if (index >= 0) {
+        r1 = {
+          productid: rData.products[index].productId,
+          productname: rData.products[index].productName,
+          category: rData.products[index].category,
+          description: rData.products[index].description,
+        };
+        res.send(r1);
+      } else {
+        res.status(404).send("Not found");
+      }
+    }
+  });
+});
+app.get("/purchases", function (req, res) {
+  let shop = +req.query.shop;
+  let product = req.query.product;
+  let sort = req.query.sort;
+  fs.readFile(fname, "utf-8", function (err, data) {
+    if (err) res.status(404).send(err);
+    else {
+      let rData = JSON.parse(data);
+      let { purchases } = rData;
+      let purchasesArr = [...purchases];
+      if (shop) {
+        purchasesArr = purchasesArr.filter((pr) => pr.shopId == shop);
+      }
+      if (product) {
+        let productArr = product.split(",");
+        purchasesArr = purchasesArr.filter((pr) =>
+          productArr.find((p) => p == pr.productid)
+        );
+      }
+      if (sort == "QtyAsc")
+        purchasesArr.sort((p1, p2) => p1.quantity - p2.quantity);
+      if (sort == "QtyDesc")
+        purchasesArr.sort((p1, p2) => p2.quantity - p1.quantity);
+      if (sort == "ValueAsc")
+        purchasesArr.sort(
+          (p1, p2) => p1.quantity * p1.price - p2.quantity * p2.price
+        );
+      if (sort == "ValueDesc")
+        purchasesArr.sort(
+          (p1, p2) => p2.quantity * p2.price - p1.quantity * p1.price
+        );
+      let r1 = purchasesArr.map((curr) => ({
+        purchaseid: curr.purchaseId,
+        shopid: curr.shopId,
+        productid: curr.productid,
+        quantity: curr.quantity,
+        price: curr.price,
+      }));
+      res.send(r1);
+    }
+  });
+});
+app.get("/purchases/shops/:id", function (req, res) {
+  let id = +req.params.id;
+  fs.readFile(fname, "utf-8", function (err, data) {
+    if (err) res.status(404).send(err);
+    else {
+      let rData = JSON.parse(data);
+      let { purchases } = rData;
+      let p1 = purchases.filter((ct) => ct.shopId == id);
+
+      let p2 = p1.map((curr) => ({
+        purchaseid: curr.purchaseId,
+        shopid: curr.shopId,
+        productid: curr.productid,
+        quantity: curr.quantity,
+        price: curr.price,
+      }));
+      res.send(p2);
+    }
+  });
+});
+app.get("/purchases/products/:id", function (req, res) {
+  let id = req.params.id;
+  fs.readFile(fname, "utf-8", function (err, data) {
+    if (err) res.status(404).send(err);
+    else {
+      let rData = JSON.parse(data);
+      let { purchases } = rData;
+      let p1 = purchases.filter((ct) => ct.productid == id);
+      let p2 = p1.map((curr) => ({
+        purchaseid: curr.purchaseId,
+        shopid: curr.shopId,
+        productid: curr.productid,
+        quantity: curr.quantity,
+        price: curr.price,
+      }));
+      res.send(p2);
+    }
+  });
+});
+app.get("/totalpurchase/shop/:id", function (req, res) {
+  let id = +req.params.id;
+  fs.readFile(fname, "utf-8", function (err, data) {
+    if (err) res.status(404).send(err);
+    else {
+      let rData = JSON.parse(data);
+      let { products, purchases } = rData;
+
+      let p1 = products.map((pr) => {
+        let prQ = purchases.reduce(
+          (acc, curr) =>
+            curr.productid == pr.productId && curr.shopId == id
+              ? +curr.quantity + acc
+              : acc,
+          0
+        );
+        console.log(prQ);
+        let prA = purchases.reduce(
+          (acc, curr) =>
+            curr.productid == pr.productId && curr.shopId == id
+              ? +curr.quantity * +curr.price + acc
+              : acc,
+          0
+        );
+        let newJson = {
+          productname: pr.productName,
+          totalquantity: prQ,
+          totalamount: prA,
+        };
+        return newJson;
+      });
+      //   console.log(p1);
+      res.send(p1);
+    }
+  });
+});
+app.get("/totalpurchase/product/:id", function (req, res) {
+  let id = +req.params.id;
+  fs.readFile(fname, "utf-8", function (err, data) {
+    if (err) res.status(404).send(err);
+    else {
+      let rData = JSON.parse(data);
+      let { shops, purchases } = rData;
+      let p1 = shops.map((pr) => {
+        let prQ = purchases.reduce(
+          (acc, curr) =>
+            curr.shopId == pr.shopId && curr.productid == id
+              ? +curr.quantity + acc
+              : acc,
+          0
+        );
+        let prA = purchases.reduce(
+          (acc, curr) =>
+            curr.shopId == pr.shopId && curr.productid == id
+              ? +curr.quantity * +curr.price + acc
+              : acc,
+          0
+        );
+        let newJson = {
+          shopname: pr.name,
+          totalquantity: prQ,
+          totalamount: prA,
+        };
+        return newJson;
+      });
+      res.send(p1);
+    }
+  });
+});
+
+app.post("/shops", function (req, res) {
+  let body = req.body;
+  fs.readFile(fname, "utf-8", function (err, data) {
+    if (err) res.status(404).send(err);
+    else {
+      let rData = JSON.parse(data);
+      let maxId = rData.shops.reduce((acc, curr) =>
+        curr.shopId > acc.shopId ? curr : acc
+      ).shopId;
+      let b1 = { shopId: +maxId + 1, name: body.shopname, rent: body.rent };
+      rData.shops.push(b1);
+      let newData = JSON.stringify(rData);
+      fs.writeFile(fname, newData, function (err) {
+        if (err) res.status(404).send(err);
+        else res.send(body);
+      });
+    }
+  });
+});
+app.post("/products", function (req, res) {
+  let body = req.body;
+  fs.readFile(fname, "utf-8", function (err, data) {
+    if (err) res.status(404).send(err);
+    else {
+      let rData = JSON.parse(data);
+      let maxId = rData.products.reduce((acc, curr) =>
+        curr.productId > acc.productId ? curr : acc
+      ).productId;
+      let b1 = {
+        productId: +maxId + 1,
+        productName: body.productname,
+        category: body.category,
+        description: body.description,
+      };
+      rData.products.push(b1);
+      let newData = JSON.stringify(rData);
+      fs.writeFile(fname, newData, function (err) {
+        if (err) res.status(404).send(err);
+        else res.send(body);
+      });
+    }
+  });
+});
+app.put("/products/:id", function (req, res) {
+  let id = +req.params.id;
+  let body = req.body;
+  fs.readFile(fname, "utf-8", function (err, data) {
+    if (err) res.status(404).send(err);
+    else {
+      let rData = JSON.parse(data);
+      let index = rData.products.findIndex((ct) => ct.productId == id);
+      if (index >= 0) {
+        let b1 = {
+          productId: id,
+          productName: body.productname,
+          category: body.category,
+          description: body.description,
+        };
+        rData.products[index] = b1;
+        let newData = JSON.stringify(rData);
+        fs.writeFile(fname, newData, function (err) {
+          if (err) res.status(404).send(err);
+          else res.send(body);
+        });
+      } else {
+        res.status(404).send("Not found");
+      }
+    }
+  });
+});
---
+++ server2/app.js.js
@@ -0,0 +1,250 @@
+const { Client } = require("pg");
+const express = require("express");
+
+const client = new Client({
+  user: "postgres",
+  password: "Ashsaclp@890",
+  database: "postgres",
+  port: 5432,
+  host: "db.owpnlsvnbbbswqctzcqk.supabase.co",
+  ssl: { rejectUnauthorized: false },
+});
+
+client.connect(function (err) {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+  } else {
+    console.log("Connected to the database!");
+  }
+});
+
+const app = express();
+app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE, HEAD"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+const port = process.env.PORT || 2410;
+app.listen(port, () => console.log(`Node app listening on port ${port}!`));
+
+
+
+app.get("/shops", function (req, res) {
+  let sql = "SELECT * FROM shops";
+  client.query(sql, function (err, result) {
+    if (err) {
+      console.log(err);
+      res.status(404).send("Error in fetching Data");
+    }  else {
+      res.send(result.rows);
+    }
+  });
+});
+app.get("/purchases/shops/:id", function (req, res) {
+  let id = req.params.id;
+  let sql = "SELECT * FROM purchases WHERE shopId=$1";
+  client.query(sql, [id], function (err, result) {
+    if (err) {
+      console.log(err);
+      res.status(404).send("Error in fetching Data");
+    }  else {
+      res.send(result.rows);
+    }
+  });
+});
+app.get("/purchases/products/:id", function (req, res) {
+  let id = req.params.id;
+  let sql = "SELECT * FROM purchases WHERE productId=$1";
+  client.query(sql, [id], function (err, result) {
+    if (err) {
+      console.log(err);
+      res.status(404).send("Error in fetching Data");
+    }  else {
+      res.send(result.rows);
+    }
+  });
+});
+app.get("/totalpurchase/shop/:id", function (req, res) {
+  let id = req.params.id;
+  let sql = `SELECT p.productName, 
+  COALESCE(SUM(pr.quantity),0) AS totalQuantity,
+  COALESCE(SUM(pr.quantity * pr.price),0) AS totalAmount
+ FROM
+  Purchases pr
+ RIGHT JOIN
+  Products p ON pr.productid = p.productId
+ AND
+  pr.shopId = $1 GROUP BY p.productId,p.productName `;
+  client.query(sql, [id], function (err, result) {
+    if (err) {
+      console.log(err);
+      res.status(404).send("Error in fetching Data");
+    }  else {
+      res.send(result.rows);
+    }
+  });
+});
+app.get("/totalpurchase/product/:id", function (req, res) {
+  let id = req.params.id;
+  let sql = `SELECT
+  s.shopName,
+  COALESCE(SUM(pr.quantity),0) AS totalQuantity,
+  COALESCE(SUM(pr.quantity * pr.price),0) AS totalAmount
+FROM
+  Purchases pr
+RIGHT JOIN
+  Shops s ON pr.shopId = s.shopId
+AND
+  pr.productid = $1
+GROUP BY
+  s.shopId, s.shopName `;
+  client.query(sql, [id], function (err, result) {
+    if (err) {
+      console.log(err);
+      res.status(404).send("Error in fetching Data");
+    } else {
+      res.send(result.rows);
+    }
+  });
+});
+
+app.get("/products", function (req, res) {
+  let brand = req.params.brand;
+  let sql = "SELECT * FROM products";
+  client.query(sql, function (err, result) {
+    if (err) {
+      console.log(err);
+      res.status(404).send("Error in fetching Data");
+    }  else {
+      res.send(result.rows);
+    }
+  });
+});
+app.get("/products/:id", function (req, res) {
+  let id = req.params.id;
+  let sql = "SELECT * FROM products WHERE productid=$1";
+  client.query(sql, [id], function (err, result) {
+    if (err) {
+      console.log(err);
+      res.status(404).send("Error in fetching Data");
+    } else if (result.rows.length === 0) {
+      res.status(404).send("No mobile found");
+    } else {
+      res.send(result.rows[0]);
+    }
+  });
+});
+app.get("/purchases", function (req, res) {
+  let shop = req.query.shop;
+  let product = req.query.product;
+  let sort = req.query.sort;
+  let option = "";
+  let optionArr = [];
+  if (shop) {
+    option = option
+      ? `${option} AND shopId=$${optionArr.length + 1}`
+      : ` WHERE shopId=$${optionArr.length + 1} `;
+    optionArr.push(shop);
+  }
+  if (product) {
+    let productArr = product.split(",");
+    option = option
+      ? `${option} AND productId IN (${productArr
+          .map((pr, index) => `$${index + optionArr.length + 1}`)
+          .join(", ")}) `
+      : ` WHERE productId IN (${productArr
+          .map((pr, index) => `$${index + 1}`)
+          .join(", ")})`;
+    productArr.map((pr) => optionArr.push(pr));
+  }
+ 
+  let sql = `SELECT * FROM purchases ${option}`;
+  if (sort == "QtyAsc") sql += " ORDER BY quantity ASC";
+  if (sort == "QtyDesc") sql += " ORDER BY quantity DESC";
+  if (sort == "ValueAsc") sql += " ORDER BY quantity * price ASC";
+  if (sort == "ValueDesc") sql += " ORDER BY quantity * price DESC";
+  client.query(sql, optionArr, function (err, result) {
+    if (err) {
+      console.log(err);
+      res.status(404).send("Error in fetching Data");
+    }  else {
+      res.send(result.rows);
+    }
+  });
+});
+
+app.post("/shops", function (req, res) {
+  let body = req.body;
+  let sql = `INSERT INTO shops(shopname, rent) VALUES ($1, $2)`;
+  client.query(sql, [body.shopname, body.rent], function (err, result) {
+    if (err) {
+      console.log(err);
+      res.status(404).send("Error in inserting Data");
+    } else {
+      res.send(result);
+    }
+  });
+});
+app.post("/products", function (req, res) {
+  let body = req.body;
+  let sql = `INSERT INTO products(productName, category,description) VALUES ($1, $2,$3)`;
+  client.query(
+    sql,
+    [body.productname, body.category, body.description],
+    function (err, result) {
+      if (err) {
+        console.log(err);
+        res.status(404).send("Error in inserting Data");
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+app.post("/purchases", function (req, res) {
+  let body = req.body;
+  let sql = `INSERT INTO purchases(shopId,productId,quantity,price) VALUES ($1, $2, $3,$4)`;
+  client.query(
+    sql,
+    [body.shopId, body.productId, quantity, price],
+    function (err, result) {
+      if (err) {
+        console.log(err);
+        res.status(404).send("Error in inserting Data");
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+app.put("/products/:id", function (req, res) {
+  let id = req.params.id;
+  let body = req.body;
+  let sql = `UPDATE products SET productname=$1, category=$2, description=$3 WHERE productid=$4`;
+  client.query(
+    sql,
+    [body.productname, body.category, body.description, id],
+    function (err, result) {
+      if (err) {
+        console.log(err);
+        res.status(404).send("Error in updating Data");
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+// ...
+
